89036d8c9ca19dc421cfec37df39f89c57ff50e0
emc7co-dot <emc7co@gmail.com>
2026-08-21 14:57:06 +0330

    Update main.dart

--- eyecare_app/lib/main.dart
+++ eyecare_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'native_screen_time_service.dart';
 
 void main() {
